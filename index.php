7bd1c6dbcafa12cd5f17cc1317a58e4c016c1030
alexeymsc <lyapinmsc@gmail.com>
2021-01-07 17:23:42 +0300

    Добавили ограничение длины текста в посте

--- index.php
+++ index.php
@@ -3,44 +3,66 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Alexey Lyapin'; // укажите здесь ваше имя
 
-$posts = [
+$posts = 
     [
-    'header' => 'Цитата',
-    'type'  => 'post-quote', 
-    'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-    'username' =>'Лариса',
-    'avatar' =>'userpic-larisa-small.jpg'    
-    ],
-    [
-        'header' => 'Игра престолов',
-        'type'  => 'post-text', 
-        'content' => 'Не могу дождаться начала финального сезона своего любимого сериала!',
-        'username' =>'Владик',
-        'avatar' =>'userpic.jpg'    
-    ],
-    [
-        'header' => 'Наконец, обработал фотки!',
-        'type'  => 'post-photo', 
-        'content' => 'rock-medium.jpg',
-        'username' =>'Виктор',
-        'avatar' =>'userpic-mark.jpg'    
-    ],
-    [
-        'header' => 'Моя мечта',
-        'type'  => 'post-photo', 
-        'content' => 'coast-medium.jpg',
-        'username' =>'Лариса',
-        'avatar' =>'userpic-larisa-small.jpg'    
-    ],    
-    [
-        'header' => 'Лучшие курсы',
-        'type'  => 'post-link', 
-        'content' => 'www.htmlacademy.ru',
-        'username' =>'Владик',
-        'avatar' =>'userpic.jpg'
-    ]
+            [
+            'header' => 'Цитата',
+            'type'  => 'post-quote', 
+            'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+            'username' =>'Лариса',
+            'avatar' =>'userpic-larisa-small.jpg'    
+            ],
+            [
+                'header' => 'Игра престолов',
+                'type'  => 'post-text', 
+                'content' => 'Озеро Байкал – огромное древнее озеро в горах Сибири к северу от монгольской границы. Байкал считается самым глубоким озером в мире. Он окружен сетью пешеходных маршрутов, называемых Большой байкальской тропой. Деревня Листвянка, расположенная на западном берегу озера, – популярная отправная точка для летних экскурсий. Зимой здесь можно кататься на коньках и собачьих упряжках.',
+                'username' =>'Владик',
+                'avatar' =>'userpic.jpg'    
+            ],
+            [
+                'header' => 'Наконец, обработал фотки!',
+                'type'  => 'post-photo', 
+                'content' => 'rock-medium.jpg',
+                'username' =>'Виктор',
+                'avatar' =>'userpic-mark.jpg'    
+            ],
+            [
+                'header' => 'Моя мечта',
+                'type'  => 'post-photo', 
+                'content' => 'coast-medium.jpg',
+                'username' =>'Лариса',
+                'avatar' =>'userpic-larisa-small.jpg'    
+            ],    
+            [
+                'header' => 'Лучшие курсы',
+                'type'  => 'post-link', 
+                'content' => 'www.htmlacademy.ru',
+                'username' =>'Владик',
+                'avatar' =>'userpic.jpg'
+            ]
 
-]
+    ];
+
+
+
+function cutContent ($long_content, $max_length =300) {
+    $current_length = mb_strlen($long_content);
+
+    if ($current_length>$max_length) {
+         
+        $long_content = explode (' ', $long_content);
+        while ($current_length >$max_length) {
+            $word = array_pop($long_content);
+            $current_length-=(mb_strlen($word)+1);
+        }
+        $short_content = implode(' ',$long_content).'...';    
+    } 
+    else {$short_content = $long_content;}
+
+    return $short_content;
+}
+
+?>
 
 
 ?>
@@ -284,7 +306,12 @@ $posts = [
                         </div>
 
                     <?php else:?>
-                        <p><?=$post['content']?></p>
+                        
+                         
+                        <p><?=cutContent($post['content'])?></p>
+                        <?php if (cutContent($post['content'])!=$post['content']):?>
+                            <a class="post-text__more-link" href="#">Читать далее</a>
+                        <?php endif ?>
 
 
                     <?php endif ?>
