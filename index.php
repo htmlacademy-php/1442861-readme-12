1c6d5635c91ab17f35a43db97838dfcc85245166
alexeymsc <lyapinmsc@gmail.com>
2021-01-07 17:33:02 +0300

    Скорректировано оформление массивов
    minor change
    
    Скорректировано оформление массивов

--- index.php
+++ index.php
@@ -6,40 +6,40 @@ $user_name = 'Alexey Lyapin'; // укажите здесь ваше имя
 $posts = 
     [
             [
-            'header' => 'Цитата',
-            'type'  => 'post-quote', 
-            'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
-            'username' =>'Лариса',
-            'avatar' =>'userpic-larisa-small.jpg'    
+                'header' => 'Цитата',
+                'type'  => 'post-quote', 
+                'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+                'username' =>'Лариса',
+                'avatar' =>'userpic-larisa-small.jpg',
             ],
             [
                 'header' => 'Игра престолов',
                 'type'  => 'post-text', 
                 'content' => 'Озеро Байкал – огромное древнее озеро в горах Сибири к северу от монгольской границы. Байкал считается самым глубоким озером в мире. Он окружен сетью пешеходных маршрутов, называемых Большой байкальской тропой. Деревня Листвянка, расположенная на западном берегу озера, – популярная отправная точка для летних экскурсий. Зимой здесь можно кататься на коньках и собачьих упряжках.',
                 'username' =>'Владик',
-                'avatar' =>'userpic.jpg'    
+                'avatar' =>'userpic.jpg',
             ],
             [
                 'header' => 'Наконец, обработал фотки!',
                 'type'  => 'post-photo', 
                 'content' => 'rock-medium.jpg',
                 'username' =>'Виктор',
-                'avatar' =>'userpic-mark.jpg'    
+                'avatar' =>'userpic-mark.jpg',  
             ],
             [
                 'header' => 'Моя мечта',
                 'type'  => 'post-photo', 
                 'content' => 'coast-medium.jpg',
                 'username' =>'Лариса',
-                'avatar' =>'userpic-larisa-small.jpg'    
+                'avatar' =>'userpic-larisa-small.jpg',   
             ],    
             [
                 'header' => 'Лучшие курсы',
                 'type'  => 'post-link', 
                 'content' => 'www.htmlacademy.ru',
                 'username' =>'Владик',
-                'avatar' =>'userpic.jpg'
-            ]
+                'avatar' =>'userpic.jpg',
+            ],
 
     ];
 
